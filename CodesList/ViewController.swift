30313cd2a7cbaffc496c1f8edd2a428784c15b2f
Briant <briantgarza@my.cwi.edu>
2021-10-21 10:33:09 -0600

    Initial Commit

---
+++ CodesList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodesList
+//
+//  Created by Briant Garza on 10/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
