6c133e040c41d43d2ced83754f4faa4f41776d38
antoniomlugo <antoniolugo@my.cwi.edu>
2021-11-11 10:42:58 -0700

    bhoi

---
+++ codesList/homeScreenViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  codesList
+//
+//  Created by CPSC_Admin01 on 10/21/21.
+//
+
+import UIKit
+
+class homeScreenViewController: UIViewController {
+    
+    @IBOutlet var userPageTapped: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ codesList/userPageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  codesList
+//
+//  Created by CPSC_Admin01 on 10/21/21.
+//
+
+import UIKit
+
+class userPageViewController: UIViewController {
+    
+    @IBOutlet var userBio: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+        place
+    
+}
+
