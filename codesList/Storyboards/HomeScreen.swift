84710914f518e3a562ef0d6b97101827dd4a8d81
Briant <briantgarza@my.cwi.edu>
2021-11-11 12:11:14 -0700

    Started Home Screen

---
+++ codesList/Storyboards/HomeScreen.swift
@@ -0,0 +1,29 @@
+//
+//  HomeScreen.swift
+//  codesList
+//
+//  Created by Briant Garza on 11/11/21.
+//
+
+import UIKit
+
+class HomeScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        initBackgroundColor(view)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- codesList/View Controllers/FavoritesScreen.swift
+++ codesList/View Controllers/FavoritesScreen.swift
@@ -7,36 +7,37 @@
 
 import UIKit
 
+func initBackgroundColor(_ view: UIView) {
+    // Used to change the background gradient color
+    let gradientLayer = CAGradientLayer()
+    
+    gradientLayer.frame = view.bounds
+    
+    gradientLayer.colors = [
+        UIColor(red: 49/255, green: 192/255, blue: 191/255, alpha: 1).cgColor,
+        UIColor(red: 156/255, green: 237/255, blue: 233/255, alpha: 1).cgColor,
+        UIColor(red: 221/255, green: 220/255, blue: 175/255, alpha: 1).cgColor
+    ]
+    
+    gradientLayer.shouldRasterize = true
+    
+    view.layer.insertSublayer(gradientLayer, at: 0)
+    
+}
+
 
 class FavoritesScreen: UIViewController {
     
     @IBOutlet weak var message: UIImageView!
-    @IBOutlet weak var favTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        initBackgroundColor()
+        initBackgroundColor(view)
         
         
     }
     
     
-    private func initBackgroundColor() {
-        // Used to change the background gradient color
-        let gradientLayer = CAGradientLayer()
-        
-        gradientLayer.frame = view.bounds
-        
-        gradientLayer.colors = [
-            UIColor(red: 49/255, green: 192/255, blue: 191/255, alpha: 1).cgColor,
-            UIColor(red: 156/255, green: 237/255, blue: 233/255, alpha: 1).cgColor,
-            UIColor(red: 221/255, green: 220/255, blue: 175/255, alpha: 1).cgColor
-        ]
-        
-        gradientLayer.shouldRasterize = true
-        
-        view.layer.insertSublayer(gradientLayer, at: 0)
-        
-    }
+    
 }
